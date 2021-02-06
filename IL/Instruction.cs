3e7209a9d0a81eb1568b71236006aefb6e2eb304
Jeff Walker <Jeff@WalkerCodeRanger.com>
2021-02-06 09:34:49 -0800

    work on instruction structure

--- IL/Instruction.cs
+++ IL/Instruction.cs
@@ -4,13 +4,35 @@ namespace Azoth.Tools.Bootstrap.IL
 {
     public readonly struct Instruction : IEquatable<Instruction>
     {
-        private readonly int value;
+        private const int Segment1Offset = 24;
+        private const uint Segment2Mask = 0x00FFF000;
+        private const int Segment2Offset = 12;
+        private const uint Segment3Mask = 0x00000FFF;
+        private const uint MaxOperandValue = 0x00000FFF;
+        private const uint ValueMask = 0x00FFFFFF;
 
-        public Instruction(OpCode opCode, int operand1, int operand2)
+        private readonly uint value;
+
+        public Instruction(ShortOpcode shortOpcode, ushort operand1, ushort operand2)
         {
-            // TODO correctly build instruction
-            value = (int)opCode + operand1 + operand2;
+            if (operand1 > MaxOperandValue)
+                throw new ArgumentException("Invalid operand value", nameof(operand1));
+            if (operand2 > MaxOperandValue)
+                throw new ArgumentException("Invalid operand value", nameof(operand2));
+            value = (uint)shortOpcode << Segment1Offset | (uint)operand1 << Segment2Offset | operand2;
         }
+
+        public ShortOpcode ShortOpcode => (ShortOpcode)(value >> Segment1Offset);
+        public ushort Operand1 => (ushort)((value & Segment2Mask) >> Segment2Offset);
+        public ushort Operand2 => (ushort)(value & Segment3Mask);
+        public uint UnsignedValue => value & ValueMask;
+        /// <remarks>Shift up unsigned, then shift down signed to get sign extension</remarks>
+        public int SignedValue => (int)((value & ValueMask) << 8) >> 8;
+
+        public UnaryOpcode UnaryOpcode => (UnaryOpcode)((value & Segment2Mask) >> Segment2Offset);
+        public ushort UnaryOperand => (ushort)(value & Segment3Mask);
+
+        public NullaryOpcode NullaryOpcode => (NullaryOpcode)(value & Segment3Mask);
 
         public bool Equals(Instruction other)
         {
@@ -19,7 +41,7 @@ namespace Azoth.Tools.Bootstrap.IL
 
         public override bool Equals(object? obj)
         {
-            return obj is Instruction other && Equals(other);
+            return obj is Instruction other && value == other.value;
         }
 
         public override int GetHashCode()
@@ -29,12 +51,12 @@ namespace Azoth.Tools.Bootstrap.IL
 
         public static bool operator ==(Instruction left, Instruction right)
         {
-            return left.Equals(right);
+            return left.value == right.value;
         }
 
         public static bool operator !=(Instruction left, Instruction right)
         {
-            return !left.Equals(right);
+            return left.value != right.value;
         }
     }
 }
--- IL/ShortOpcode.cs
+++ IL/ShortOpcode.cs
@@ -3,9 +3,9 @@ using System.Diagnostics.CodeAnalysis;
 namespace Azoth.Tools.Bootstrap.IL
 {
     [SuppressMessage("Design", "CA1028:Enum Storage should be Int32",
-        Justification = "OpCodes are stored as bytes")]
-    public enum OpCode : byte
+        Justification = "Short Opcodes are stored as bytes")]
+    public enum ShortOpcode : byte
     {
-        None
+        None = 0,
     }
 }
