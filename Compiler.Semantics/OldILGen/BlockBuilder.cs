26e99eb7c5a5da0bf180da570135034734c5bf25
Jeff Walker <Jeff@WalkerCodeRanger.com>
2021-02-15 13:41:49 -0800

    remove references to obsolete IntermediateLanguage project

--- Compiler.Semantics/OldILGen/BlockBuilder.cs
+++ Compiler.Semantics/OldILGen/BlockBuilder.cs
@@ -1,40 +1,33 @@
-using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.Diagnostics.CodeAnalysis;
-using Azoth.Tools.Bootstrap.Compiler.IntermediateLanguage.CFG.Instructions;
-using Azoth.Tools.Bootstrap.Compiler.IntermediateLanguage.CFG.TerminatorInstructions;
-
 namespace Azoth.Tools.Bootstrap.Compiler.Semantics.OldILGen
 {
-    [DebuggerDisplay("{" + nameof(DebuggerDisplay) + ",nq}")]
-    internal class BlockBuilder
-    {
-        public int Number { get; }
-        private readonly List<Instruction> instructions = new List<Instruction>();
-        public IReadOnlyList<Instruction> Instructions => instructions;
-        public bool IsTerminated => Terminator != null;
-        [DisallowNull]
-        public TerminatorInstruction? Terminator { get; private set; }
+    //[DebuggerDisplay("{" + nameof(DebuggerDisplay) + ",nq}")]
+    //internal class BlockBuilder
+    //{
+    //    public int Number { get; }
+    //    private readonly List<Instruction> instructions = new List<Instruction>();
+    //    public IReadOnlyList<Instruction> Instructions => instructions;
+    //    public bool IsTerminated => Terminator != null;
+    //    [DisallowNull]
+    //    public TerminatorInstruction? Terminator { get; private set; }
 
-        public BlockBuilder(in int number)
-        {
-            Number = number;
-        }
+    //    public BlockBuilder(in int number)
+    //    {
+    //        Number = number;
+    //    }
 
-        public void Add(Instruction instruction)
-        {
-            instructions.Add(instruction);
-        }
+    //    public void Add(Instruction instruction)
+    //    {
+    //        instructions.Add(instruction);
+    //    }
 
-        public void End(TerminatorInstruction instruction)
-        {
-            if (Terminator != null)
-                throw new InvalidOperationException("Can't set terminator instruction more than once");
-            Terminator = instruction;
-        }
+    //    public void End(TerminatorInstruction instruction)
+    //    {
+    //        if (Terminator != null)
+    //            throw new InvalidOperationException("Can't set terminator instruction more than once");
+    //        Terminator = instruction;
+    //    }
 
-        [DebuggerBrowsable(DebuggerBrowsableState.Never)]
-        private string DebuggerDisplay => $"BB #{Number}, Instructions={Instructions.Count + (IsTerminated ? 1 : 0)}, Terminated={IsTerminated}";
-    }
+    //    [DebuggerBrowsable(DebuggerBrowsableState.Never)]
+    //    private string DebuggerDisplay => $"BB #{Number}, Instructions={Instructions.Count + (IsTerminated ? 1 : 0)}, Terminated={IsTerminated}";
+    //}
 }
--- Compiler.Semantics/OldILGen/ControlFlowGraphBuilder.cs
+++ Compiler.Semantics/OldILGen/ControlFlowGraphBuilder.cs
@@ -1,111 +1,100 @@
-using System;
-using System.Collections.Generic;
-using System.Diagnostics.CodeAnalysis;
-using System.Linq;
-using Azoth.Tools.Bootstrap.Compiler.Core;
-using Azoth.Tools.Bootstrap.Compiler.IntermediateLanguage.CFG;
-using Azoth.Tools.Bootstrap.Compiler.IntermediateLanguage.CFG.TerminatorInstructions;
-using Azoth.Tools.Bootstrap.Compiler.Symbols;
-using Azoth.Tools.Bootstrap.Compiler.Types;
-using Azoth.Tools.Bootstrap.Framework;
-
 namespace Azoth.Tools.Bootstrap.Compiler.Semantics.OldILGen
 {
     /// <summary>
     /// Builder pattern for control flow graphs used during control flow graph fabrication
     /// </summary>
-    internal class ControlFlowGraphBuilder
-    {
-        private readonly CodeFile file;
-        private readonly List<VariableDeclaration> variables = new List<VariableDeclaration>();
-        public VariableDeclaration SelfVariable => variables.SingleOrDefault(v => v.Variable == Variable.Self);
-        private readonly List<BlockBuilder> blockBuilders = new List<BlockBuilder>();
+    //internal class ControlFlowGraphBuilder
+    //{
+    //    private readonly CodeFile file;
+    //    private readonly List<VariableDeclaration> variables = new List<VariableDeclaration>();
+    //    public VariableDeclaration SelfVariable => variables.SingleOrDefault(v => v.Variable == Variable.Self);
+    //    private readonly List<BlockBuilder> blockBuilders = new List<BlockBuilder>();
 
-        public ControlFlowGraphBuilder(CodeFile file)
-        {
-            this.file = file;
-        }
+    //    public ControlFlowGraphBuilder(CodeFile file)
+    //    {
+    //        this.file = file;
+    //    }
 
-        public VariableDeclaration AddVariable(bool mutableBinding, DataType type, Scope scope, BindingSymbol? symbol = null)
-        {
-            var variable = new VariableDeclaration(false, mutableBinding, type.ToNonConstantType(),
-                            new Variable(variables.Count), scope, symbol);
-            variables.Add(variable);
-            return variable;
-        }
+    //    public VariableDeclaration AddVariable(bool mutableBinding, DataType type, Scope scope, BindingSymbol? symbol = null)
+    //    {
+    //        var variable = new VariableDeclaration(false, mutableBinding, type.ToNonConstantType(),
+    //                        new Variable(variables.Count), scope, symbol);
+    //        variables.Add(variable);
+    //        return variable;
+    //    }
 
-        public VariableDeclaration AddParameter(bool mutableBinding, DataType type, Scope scope, BindingSymbol symbol)
-        {
-            var variable = new VariableDeclaration(true, mutableBinding, type.ToNonConstantType(),
-                            new Variable(variables.Count), scope, symbol);
-            variables.Add(variable);
-            return variable;
-        }
+    //    public VariableDeclaration AddParameter(bool mutableBinding, DataType type, Scope scope, BindingSymbol symbol)
+    //    {
+    //        var variable = new VariableDeclaration(true, mutableBinding, type.ToNonConstantType(),
+    //                        new Variable(variables.Count), scope, symbol);
+    //        variables.Add(variable);
+    //        return variable;
+    //    }
 
-        public VariableDeclaration AddSelfParameter(SelfParameterSymbol symbol)
-        {
-            Requires.That("variableNumber", variables.Count == 0, "Self parameter must have variable number 0");
-            var variable = new VariableDeclaration(true, false, symbol.DataType, Variable.Self, null, symbol);
-            variables.Add(variable);
-            return variable;
-        }
+    //    public VariableDeclaration AddSelfParameter(SelfParameterSymbol symbol)
+    //    {
+    //        Requires.That("variableNumber", variables.Count == 0, "Self parameter must have variable number 0");
+    //        var variable = new VariableDeclaration(true, false, symbol.DataType, Variable.Self, null, symbol);
+    //        variables.Add(variable);
+    //        return variable;
+    //    }
 
-        public VariableDeclaration Let(DataType type, Scope scope)
-        {
-            return AddVariable(false, type, scope);
-        }
+    //    public VariableDeclaration Let(DataType type, Scope scope)
+    //    {
+    //        return AddVariable(false, type, scope);
+    //    }
 
-        public VariableDeclaration Var(DataType type, Scope scope)
-        {
-            return AddVariable(true, type, scope);
-        }
+    //    public VariableDeclaration Var(DataType type, Scope scope)
+    //    {
+    //        return AddVariable(true, type, scope);
+    //    }
 
-        public VariableDeclaration VariableFor(BindingSymbol symbol)
-        {
-            return variables.Single(v => v.Symbol == symbol);
-        }
+    //    public VariableDeclaration VariableFor(BindingSymbol symbol)
+    //    {
+    //        return variables.Single(v => v.Symbol == symbol);
+    //    }
 
-        public BlockBuilder NewBlock()
-        {
-            var block = new BlockBuilder(blockBuilders.Count);
-            blockBuilders.Add(block);
-            return block;
-        }
+    //    public BlockBuilder NewBlock()
+    //    {
+    //        var block = new BlockBuilder(blockBuilders.Count);
+    //        blockBuilders.Add(block);
+    //        return block;
+    //    }
 
-        /// Used to create a new block for the entry into a loop if needed.
-        /// Doesn't create a new block if the current block is empty.
-        public BlockBuilder NewEntryBlock(BlockBuilder currentBlock, TextSpan span, Scope scope)
-        {
-            if (!currentBlock.IsTerminated && !currentBlock.Instructions.Any()) return currentBlock;
-            var entryBlock = NewBlock();
-            currentBlock.End(new GotoInstruction(entryBlock.Number, span, scope));
-            return entryBlock;
-        }
+    //    /// Used to create a new block for the entry into a loop if needed.
+    //    /// Doesn't create a new block if the current block is empty.
+    //    public BlockBuilder NewEntryBlock(BlockBuilder currentBlock, TextSpan span, Scope scope)
+    //    {
+    //        if (!currentBlock.IsTerminated && !currentBlock.Instructions.Any()) return currentBlock;
+    //        var entryBlock = NewBlock();
+    //        currentBlock.End(new GotoInstruction(entryBlock.Number, span, scope));
+    //        return entryBlock;
+    //    }
 
-        public ControlFlowGraph Build()
-        {
-            // We assume that the first block is the entry block
-            var blocks = new List<Block>();
-            foreach (var block in blockBuilders)
-            {
-                var terminator = block.Terminator ?? throw new InvalidOperationException();
-                blocks.Add(new Block(block.Number, block.Instructions.ToFixedList(), terminator));
-            }
+    //    public ControlFlowGraph Build()
+    //    {
+    //        // We assume that the first block is the entry block
+    //        var blocks = new List<Block>();
+    //        foreach (var block in blockBuilders)
+    //        {
+    //            var terminator = block.Terminator ?? throw new InvalidOperationException();
+    //            blocks.Add(new Block(block.Number, block.Instructions.ToFixedList(), terminator));
+    //        }
 
-            return new ControlFlowGraph(file, variables, blocks);
-        }
+    //        return new ControlFlowGraph(file, variables, blocks);
+    //    }
 
-        [SuppressMessage("Design", "CA1043:Use Integral Or String Argument For Indexers", Justification =
-            "Variable is a value type, essentially a strongly type integer")]
-        public VariableDeclaration this[Variable variable]
-        {
-            get
-            {
-                var declaration = variables[variable.Number];
-                if (declaration.Variable != variable)
-                    throw new InvalidOperationException("Declaration variable isn't this variable");
-                return declaration;
-            }
-        }
-    }
+    //    [SuppressMessage("Design", "CA1043:Use Integral Or String Argument For Indexers", Justification =
+    //        "Variable is a value type, essentially a strongly type integer")]
+    //    public VariableDeclaration this[Variable variable]
+    //    {
+    //        get
+    //        {
+    //            var declaration = variables[variable.Number];
+    //            if (declaration.Variable != variable)
+    //                throw new InvalidOperationException("Declaration variable isn't this variable");
+    //            return declaration;
+    //        }
+    //    }
+    //}
 }
