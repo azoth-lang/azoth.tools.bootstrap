d56cac44e6335b4904967109782bf955ee950073
Jeff Walker <Jeff@WalkerCodeRanger.com>
2023-09-04 12:47:17 -0700

    supporting skipping test cases

--- Tests.Compiler.Conformance/Helpers/CodeFiles.cs
+++ Tests.Compiler.Conformance/Helpers/CodeFiles.cs
@@ -1,12 +1,12 @@
 using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 
 namespace Azoth.Tools.Bootstrap.Tests.Conformance.Helpers;
 
 public static class CodeFiles
 {
     public static IEnumerable<string> GetIn(string directory)
-    {
-        return Directory.EnumerateFiles(directory, "*.az", SearchOption.AllDirectories);
-    }
+        => Directory.EnumerateFiles(directory, "*.az", SearchOption.AllDirectories)
+                    .Where(p => !p.EndsWith(".skip.az"));
 }
