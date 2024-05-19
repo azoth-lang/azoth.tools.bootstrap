16950d0877dc8bc7f794db5fdca52bd4415c4c12
Jeff Walker <Jeff@WalkerCodeRanger.com>
2024-05-19 15:57:37 -0700

    remove dead code in EntitySymbolBuilder

--- Compiler.Semantics/Symbols/Entities/EntitySymbolBuilder.cs
+++ Compiler.Semantics/Symbols/Entities/EntitySymbolBuilder.cs
@@ -1,15 +1,8 @@
 using System.Collections.Generic;
-using System.Diagnostics;
 using System.Linq;
 using Azoth.Tools.Bootstrap.Compiler.AST;
-using Azoth.Tools.Bootstrap.Compiler.Core;
 using Azoth.Tools.Bootstrap.Compiler.CST;
-using Azoth.Tools.Bootstrap.Compiler.Semantics.Errors;
-using Azoth.Tools.Bootstrap.Compiler.Symbols;
 using Azoth.Tools.Bootstrap.Compiler.Symbols.Trees;
-using Azoth.Tools.Bootstrap.Compiler.Types;
-using Azoth.Tools.Bootstrap.Compiler.Types.Parameters;
-using Azoth.Tools.Bootstrap.Compiler.Types.Pseudotypes;
 using Azoth.Tools.Bootstrap.Framework;
 using ExhaustiveMatching;
 
@@ -17,46 +10,35 @@ namespace Azoth.Tools.Bootstrap.Compiler.Semantics.Symbols.Entities;
 
 public class EntitySymbolBuilder
 {
-    private readonly Diagnostics diagnostics;
     private readonly ISymbolTreeBuilder symbolTree;
     private readonly SymbolForest symbolTrees;
 
-    private EntitySymbolBuilder(Diagnostics diagnostics, ISymbolTreeBuilder symbolTree, SymbolForest symbolTrees)
+    private EntitySymbolBuilder(ISymbolTreeBuilder symbolTree, SymbolForest symbolTrees)
     {
-        this.diagnostics = diagnostics;
         this.symbolTree = symbolTree;
         this.symbolTrees = symbolTrees;
     }
 
     public static void BuildFor(PackageSyntax<Package> package)
     {
-        var builder = new EntitySymbolBuilder(package.Diagnostics, package.SymbolTree, package.SymbolTrees);
+        var builder = new EntitySymbolBuilder(package.SymbolTree, package.SymbolTrees);
         builder.Build(package.EntityDeclarations);
 
-        builder = new EntitySymbolBuilder(package.Diagnostics, package.TestingSymbolTree, package.TestingSymbolTrees);
+        builder = new EntitySymbolBuilder(package.TestingSymbolTree, package.TestingSymbolTrees);
         builder.Build(package.TestingEntityDeclarations);
     }
 
     private void Build(IFixedSet<IEntityDefinitionSyntax> entities)
     {
-        // Process all types first because they may be referenced by functions etc.
-        var typeDeclarations = entities.OfType<ITypeDefinitionSyntax>().ToFixedList();
-        foreach (var type in typeDeclarations)
-            BuildTypeSymbol(type);
-
-        // Now resolve all other symbols (type declarations will already have symbols and won't be processed again)
         foreach (var entity in entities)
-            BuildNonTypeEntitySymbol(entity);
+            BuildEntitySymbol(entity);
 
+        var typeDeclarations = entities.OfType<ITypeDefinitionSyntax>();
         var inheritor = new TypeSymbolInheritor(symbolTree, symbolTrees, typeDeclarations);
         inheritor.AddInheritedSymbols();
     }
 
-    /// <summary>
-    /// If the type has not been resolved, this resolves it. This function
-    /// also watches for type cycles and reports an error.
-    /// </summary>
-    private void BuildNonTypeEntitySymbol(IEntityDefinitionSyntax entity)
+    private void BuildEntitySymbol(IEntityDefinitionSyntax entity)
     {
         switch (entity)
         {
@@ -80,8 +62,14 @@ public class EntitySymbolBuilder
             case IFunctionDefinitionSyntax syn:
                 BuildFunctionSymbol(syn);
                 break;
-            case ITypeDefinitionSyntax _:
-                // Type declarations already processed
+            case IClassDefinitionSyntax @class:
+                BuildClassSymbol(@class);
+                break;
+            case IStructDefinitionSyntax @struct:
+                BuildStructSymbol(@struct);
+                break;
+            case ITraitDefinitionSyntax trait:
+                BuildTraitSymbol(trait);
                 break;
         }
     }
@@ -92,11 +80,9 @@ public class EntitySymbolBuilder
         var symbol = method.Symbol.Result;
         symbolTree.Add(symbol);
 
-        // EntitySymbolApplier doesn't cover parameters because they are not metadata symbols
-        var selfParameterType = symbol.SelfParameterType.Type;
-        var parameterTypes = symbol.MethodGroupType.Parameters;
-        BuildSelfParameterSymbol(symbol, method.SelfParameter, selfParameterType);
-        BuildParameterSymbols(symbol, method.File, method.Parameters, parameterTypes);
+        // Parameter symbols already set by EntitySymbolApplier
+        symbolTree.Add(method.SelfParameter.Symbol.Result);
+        BuildParameterSymbols(method.Parameters);
     }
 
     private void BuildConstructorSymbol(IConstructorDefinitionSyntax constructor)
@@ -105,11 +91,9 @@ public class EntitySymbolBuilder
         var symbol = constructor.Symbol.Result;
         symbolTree.Add(symbol);
 
-        // EntitySymbolApplier doesn't cover parameters because they are not metadata symbols
-        var selfParameterType = symbol.SelfParameterType;
-        var parameterTypes = symbol.Parameters;
-        BuildSelfParameterSymbol(symbol, constructor.SelfParameter, selfParameterType, isConstructor: true);
-        BuildParameterSymbols(symbol, constructor.File, constructor.Parameters, parameterTypes);
+        // Parameter symbols already set by EntitySymbolApplier
+        symbolTree.Add(constructor.SelfParameter.Symbol.Result);
+        BuildParameterSymbols(constructor.Parameters);
     }
 
     private void BuildInitializerSymbol(IInitializerDefinitionSyntax initializer)
@@ -118,11 +102,9 @@ public class EntitySymbolBuilder
         var symbol = initializer.Symbol.Result;
         symbolTree.Add(symbol);
 
-        // EntitySymbolApplier doesn't cover parameters because they are not metadata symbols
-        var selfParameterType = symbol.SelfParameterType;
-        var parameterTypes = symbol.Parameters;
-        BuildSelfParameterSymbol(symbol, initializer.SelfParameter, selfParameterType, isConstructor: true);
-        BuildParameterSymbols(symbol, initializer.File, initializer.Parameters, parameterTypes);
+        // Parameter symbols already set by EntitySymbolApplier
+        symbolTree.Add(initializer.SelfParameter.Symbol.Result);
+        BuildParameterSymbols(initializer.Parameters);
     }
 
     private void BuildAssociatedFunctionSymbol(IAssociatedFunctionDefinitionSyntax associatedFunction)
@@ -131,9 +113,8 @@ public class EntitySymbolBuilder
         var symbol = associatedFunction.Symbol.Result;
         symbolTree.Add(symbol);
 
-        // EntitySymbolApplier doesn't cover parameters because they are not metadata symbols
-        var parameterTypes = symbol.Type.Parameters.Select(p => p.Type);
-        BuildParameterSymbols(symbol, associatedFunction.File, associatedFunction.Parameters, parameterTypes);
+        // Parameter symbols already set by EntitySymbolApplier
+        BuildParameterSymbols(associatedFunction.Parameters);
     }
 
     private void BuildFieldSymbol(IFieldDefinitionSyntax field)
@@ -149,27 +130,8 @@ public class EntitySymbolBuilder
         var symbol = function.Symbol.Result;
         symbolTree.Add(symbol);
 
-        // EntitySymbolApplier doesn't cover parameters because they are not metadata symbols
-        var parameterTypes = symbol.Type.Parameters.Select(p => p.Type);
-        BuildParameterSymbols(symbol, function.File, function.Parameters, parameterTypes);
-    }
-
-    private void BuildTypeSymbol(ITypeDefinitionSyntax type)
-    {
-        switch (type)
-        {
-            default:
-                throw ExhaustiveMatch.Failed(type);
-            case IClassDefinitionSyntax @class:
-                BuildClassSymbol(@class);
-                break;
-            case IStructDefinitionSyntax @struct:
-                BuildStructSymbol(@struct);
-                break;
-            case ITraitDefinitionSyntax trait:
-                BuildTraitSymbol(trait);
-                break;
-        }
+        // Parameter symbols already set by EntitySymbolApplier
+        BuildParameterSymbols(function.Parameters);
     }
 
     private void BuildClassSymbol(IClassDefinitionSyntax @class)
@@ -207,58 +169,21 @@ public class EntitySymbolBuilder
     }
 
     private void BuildParameterSymbols(
-        InvocableSymbol containingSymbol,
-        CodeFile file,
-        IEnumerable<IConstructorOrInitializerParameterSyntax> parameters,
-        IEnumerable<Parameter> parameterTypes)
-        => BuildParameterSymbols(containingSymbol, file, parameters, parameterTypes.Select(pt => pt.Type));
-
-    private void BuildParameterSymbols(
-        InvocableSymbol containingSymbol,
-        CodeFile file,
-        IEnumerable<IConstructorOrInitializerParameterSyntax> parameters,
-        IEnumerable<DataType> types)
+        IEnumerable<IConstructorOrInitializerParameterSyntax> parameters)
     {
-        foreach (var (param, type) in parameters.EquiZip(types))
+        foreach (var param in parameters)
         {
             switch (param)
             {
                 default:
                     throw ExhaustiveMatch.Failed(param);
                 case INamedParameterSyntax namedParam:
-                {
-                    var isLent = namedParam.IsLentBinding;
-                    //if (isLent && !type.CanBeLent())
-                    //{
-                    //    diagnostics.Add(TypeError.TypeCannotBeLent(file, namedParam.Span, type));
-                    //    isLent = false;
-                    //}
-
-                    var expected = NamedVariableSymbol.CreateParameter(containingSymbol, namedParam.Name,
-                        namedParam.DeclarationNumber.Result, namedParam.IsMutableBinding, isLent, type);
-                    //namedParam.Symbol.Fulfill(symbol);
-                    if (namedParam.Symbol.Result != expected)
-                        throw new UnreachableException("Named parameter symbol should match expected.");
                     symbolTree.Add(namedParam.Symbol.Result);
-                }
-                break;
+                    break;
                 case IFieldParameterSyntax _:
                     // Referenced field already assigned
                     break;
             }
         }
-    }
-
-    private void BuildSelfParameterSymbol(
-        InvocableSymbol containingSymbol,
-        ISelfParameterSyntax param,
-        Pseudotype type,
-        bool isConstructor = false)
-    {
-        var expected = new SelfParameterSymbol(containingSymbol, param.IsLentBinding && !isConstructor, type);
-        //param.Symbol.Fulfill(expected);
-        if (param.Symbol.Result != expected)
-            throw new UnreachableException("Self parameter symbol should match expected.");
-        symbolTree.Add(param.Symbol.Result);
     }
 }
