a729dc2ee0a091a9ac2fc906eada1277f8b1f36c
Jeff Walker <Jeff@WalkerCodeRanger.com>
2024-07-31 16:37:14 -0700

    Add IntIsNotNone test

--- Tests.Unit.Compiler.AST.Interpreter/MemoryLayout/AzothValueTests.cs
+++ Tests.Unit.Compiler.AST.Interpreter/MemoryLayout/AzothValueTests.cs
@@ -1,3 +1,4 @@
+using System.Numerics;
 using Azoth.Tools.Bootstrap.Compiler.AST.Interpreter.MemoryLayout;
 using Xunit;
 
@@ -9,5 +10,16 @@ public class AzothValueTests
     public void NoneIsNone()
     {
         Assert.True(AzothValue.None.IsNone);
+    }
+
+    /// <summary>
+    /// Due to the memory layout of <see cref="AzothValue"/> and <see cref="BigInteger"/> a special
+    /// flag reference must be used to indicate `none`. If that isn't done, then the `IsNone`
+    /// property could incorrectly report that an integer value is `none`.
+    /// </summary>
+    [Fact]
+    public void IntIsNotNone()
+    {
+        Assert.False(AzothValue.Int(42).IsNone);
     }
 }
