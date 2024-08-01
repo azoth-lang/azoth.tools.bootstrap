5115f89dbbf64ab94fdd1e38408e1f1cf144f6ff
Jeff Walker <Jeff@WalkerCodeRanger.com>
2024-08-01 15:46:22 -0700

    delete old AST interpreter

--- Compiler.AST.Interpreter/Abort.cs
+++
@@ -1,5 +0,0 @@
-using System;
-
-namespace Azoth.Tools.Bootstrap.Compiler.AST.Interpreter;
-
-internal class Abort(string message) : Exception(message);
--- Compiler.AST.Interpreter/ControlFlow/Next.cs
+++
@@ -1,7 +0,0 @@
-using System;
-
-namespace Azoth.Tools.Bootstrap.Compiler.AST.Interpreter.ControlFlow;
-
-internal class Next : Exception
-{
-}
