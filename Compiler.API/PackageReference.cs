84bc829dc2b991c201776ca5ee100ae47100b873
Jeff Walker <Jeff@WalkerCodeRanger.com>
2024-08-01 17:04:55 -0700

    make PackageReference use IPackageSymbols

--- Compiler.API/PackageReference.cs
+++ Compiler.API/PackageReference.cs
@@ -5,12 +5,12 @@ using Azoth.Tools.Bootstrap.Compiler.Symbols;
 
 namespace Azoth.Tools.Bootstrap.Compiler.API;
 
-public sealed class PackageReference(IdentifierName nameOrAlias, IPackageSymbols package, bool isTrusted)
+public sealed class PackageReference(IdentifierName nameOrAlias, IPackageSymbols packageSymbols, bool isTrusted)
 {
     public IdentifierName NameOrAlias { get; } = nameOrAlias;
-    public IPackageSymbols Package { get; } = package;
+    public IPackageSymbols PackageSymbols { get; } = packageSymbols;
     public bool IsTrusted { get; } = isTrusted;
 
     internal IPackageReferenceSyntax ToSyntax()
-        => new PackageReferenceSyntax(NameOrAlias, Package, IsTrusted);
+        => new PackageReferenceSyntax(NameOrAlias, PackageSymbols, IsTrusted);
 }
