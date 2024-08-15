ec0438a21aae6a725fab02071c84d6f376282a13
Jeff Walker <Jeff@WalkerCodeRanger.com>
2024-08-14 23:14:59 -0700

    remove AllSupertypeNames from syntax tree

--- Compiler.Syntax/ITypeDefinitionSyntax.cs
+++
@@ -1,8 +0,0 @@
-using System.Collections.Generic;
-
-namespace Azoth.Tools.Bootstrap.Compiler.Syntax;
-
-public partial interface ITypeDefinitionSyntax
-{
-    IEnumerable<IStandardTypeNameSyntax> AllSupertypeNames => SupertypeNames;
-}
