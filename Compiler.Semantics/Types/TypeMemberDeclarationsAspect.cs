a6d80331228c95077084ff59fc19ecd4200943e0
Jeff Walker <Jeff@WalkerCodeRanger.com>
2024-08-19 21:11:13 -0700

    add empty ValueIds aspect

--- Compiler.Semantics/Types/TypeMemberDeclarationsAspect.cs
+++ Compiler.Semantics/Types/TypeMemberDeclarationsAspect.cs
@@ -1,7 +1,6 @@
 using System.Collections.Generic;
 using System.Linq;
 using Azoth.Tools.Bootstrap.Compiler.Antetypes;
-using Azoth.Tools.Bootstrap.Compiler.Core;
 using Azoth.Tools.Bootstrap.Compiler.Core.Code;
 using Azoth.Tools.Bootstrap.Compiler.Core.Diagnostics;
 using Azoth.Tools.Bootstrap.Compiler.Semantics.Errors;
