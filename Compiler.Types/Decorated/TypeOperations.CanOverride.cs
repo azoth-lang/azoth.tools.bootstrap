f9b037535289753ee4b4a6bcacbd29120f535b38
Jeff Walker <Jeff@WalkerCodeRanger.com>
2025-02-24 15:21:23 -0800

    support subtyping of functions when the capability makes `lent` drop

--- Compiler.Types/Decorated/TypeOperations.CanOverride.cs
+++ Compiler.Types/Decorated/TypeOperations.CanOverride.cs
@@ -1,3 +1,4 @@
+using Azoth.Tools.Bootstrap.Compiler.Types.Capabilities;
 using Azoth.Tools.Bootstrap.Framework;
 
 namespace Azoth.Tools.Bootstrap.Compiler.Types.Decorated;
@@ -9,5 +10,15 @@ public static partial class TypeOperations
         => baseParameterType.IsSubtypeOf(self);
 
     public static bool CanOverride(this ParameterType self, ParameterType baseParameter)
-        => baseParameter.IsLent.Implies(self.IsLent) && baseParameter.Type.IsSubtypeOf(self.Type);
+    {
+        if (baseParameter.IsLent.Implies(self.IsLent) && baseParameter.Type.IsSubtypeOf(self.Type))
+            return true;
+
+        if (baseParameter.IsLent && !self.IsLent && baseParameter.Type.IsSubtypeOf(self.Type)
+            && self.Type is CapabilityType parameterType)
+            // TODO what about independent generic parameters
+            return CapabilitySet.Shareable.AllowedCapabilities.Contains(parameterType.Capability);
+
+        return false;
+    }
 }
