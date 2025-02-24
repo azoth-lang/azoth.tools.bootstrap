ad947025e7e2e9413caba72d1b07f79e95db7399
Jeff Walker <Jeff@WalkerCodeRanger.com>
2025-02-24 13:26:32 -0800

    Make `Implies` short-circuit with inlining

--- Framework/BoolExtensions.cs
+++ Framework/BoolExtensions.cs
@@ -1,4 +1,3 @@
-using System;
 using InlineMethod;
 
 namespace Azoth.Tools.Bootstrap.Framework;
@@ -8,21 +7,17 @@ public static class BoolExtensions
     /// <summary>
     /// The logical relation implies (i.e. X implies Y means that if X is true then Y must be true).
     /// </summary>
-    /// <remarks>This method is meant to be inlined. That means the <c>InlineMethod.Fody</c> package
-    /// should be properly installed in the consuming code.</remarks>
+    /// <remarks><para>This method is meant to be inlined. That means the <c>InlineMethod.Fody</c>
+    /// package should be properly installed in the consuming code. When inlined, the method will
+    /// have short-circuiting behavior like `&&` or `||`.</para>
+    ///
+    /// <para>CAUTION: If this method is not inlined, it will not short-circuit.</para></remarks>
     [Inline(export: true)]
-    public static bool Implies(this bool self, bool other)
-        => !self || other;
-
-    /// <summary>
-    /// The logical relation implies (i.e. X implies Y means that if X is true then Y must be true).
-    /// This overload will short-circuit and not evaluate <paramref name="other"/> when not
-    /// necessary.
-    /// </summary>
-    /// <remarks>This method is meant to be inlined. That means the <c>InlineMethod.Fody</c> package
-    /// should be properly installed in the consuming code. However, a lambda parameter <i>may</i>
-    /// still not be optimized away.</remarks>
-    [Inline(export: true)]
-    public static bool Implies(this bool self, Func<bool> other)
-        => !self || other();
+    public static bool Implies(this bool self, [InlineParameter] bool other)
+    {
+        // Must use `if` instead of `||` so C# compiler will not optimize away the short-circuiting
+        // and make the inline work incorrectly.
+        if (!self) return true;
+        return other;
+    }
 }
--- Tests.Unit.Framework/BoolExtensionsTests.cs
+++ Tests.Unit.Framework/BoolExtensionsTests.cs
@@ -6,33 +6,22 @@ namespace Azoth.Tools.Bootstrap.Tests.Unit.Framework;
 public class BoolExtensionsTests
 {
     [Fact]
-    public void ImpliesWithBoolDoesNotShortCircuit()
+    public void InlinedImpliesWithBoolDoesShortCircuit()
     {
         var actual = False(out var leftCalled).Implies(True(out var rightCalled));
-
-        Assert.True(actual);
-        Assert.True(leftCalled);
-        Assert.True(rightCalled);
-    }
-
-    [Fact]
-    public void ImpliesWithFuncDoesShortCircuit()
-    {
-        bool rightCalled = false;
-        var actual = False(out var leftCalled).Implies(() => True(out rightCalled));
 
         Assert.True(actual);
         Assert.True(leftCalled);
         Assert.False(rightCalled);
     }
 
-    private bool False(out bool called)
+    private static bool False(out bool called)
     {
         called = true;
         return false;
     }
 
-    private bool True(out bool called)
+    private static bool True(out bool called)
     {
         called = true;
         return true;
