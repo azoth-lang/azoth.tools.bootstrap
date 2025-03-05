72946aa16b41d86fe6d041b48874221fb66e5ecb
Jeff Walker <Jeff@WalkerCodeRanger.com>
2025-03-04 16:04:25 -0800

    fix `RewriteContexts` to avoid prematurely marking nodes as InFinalTree

--- Compiler.Core/Attributes/RewriteContexts.cs
+++ Compiler.Core/Attributes/RewriteContexts.cs
@@ -1,39 +1,54 @@
 using System;
 using System.Collections.Generic;
+using System.Collections.Immutable;
 using System.Diagnostics;
 using System.Runtime.CompilerServices;
 using Azoth.Tools.Bootstrap.Framework;
+using InlineMethod;
 
 namespace Azoth.Tools.Bootstrap.Compiler.Core.Attributes;
 
 /// <summary>
-/// A collection of rewrite contexts.
+/// A collection of rewrite contexts for nodes.
 /// </summary>
+/// <remarks><para>It might be possible to use <see cref="ImmutableDictionary{TKey,TValue}"/> to
+/// create a nested cache at each layer so that as contexts were marked inactive one could roll back
+/// to a cache without the child rewrite context. However, that would be a large refactoring and may
+/// not improve performance.</para></remarks>
 [DebuggerStepThrough]
 internal sealed class RewriteContexts
 {
     /// <summary>
-    /// An incomplete dictionary of nodes to their rewrite contexts. This is updated as queries are
-    /// done to cache the results and improve performance of later queries.
+    /// A dictionary of nodes to their rewrite contexts.
     /// </summary>
-    /// <remarks>If an entry in the dictionary refers to an inactive context, it must be ignored and
-    /// removed.</remarks>
-    private readonly Dictionary<ITreeNode, RewriteContext> contextNodes
+    /// <remarks>If an entry in the dictionary refers to an inactive context, it does not matter for
+    /// determining the low link. However, it is still an indication that the node is not in the
+    /// final tree.</remarks>
+    private readonly Dictionary<ITreeNode, RewriteContext> contexts
         = new(ReferenceEqualityComparer.Instance);
 
     /// <summary>
-    /// Try to add a new rewrite context for the specified node.
+    /// A cache of rewrite contexts for nodes.
     /// </summary>
-    /// <returns>The newly added <see cref="RewriteContext"/> or <see langword="null"/> if it is
-    /// already under an existing context.</returns>
+    /// <remarks>If a cached context is active, it is correct. If it is inactive, then the cache
+    /// could be stale and there could be a parent context that is active. Entries are not removed
+    /// from the cache except by clearing the whole cache to avoid a situation where a parent node
+    /// is removed from the cache without child nodes being removed and thereby causing incorrect
+    /// cache clearing.</remarks>
+    private readonly Dictionary<ITreeNode, RewriteContext> contextCache
+        = new(ReferenceEqualityComparer.Instance);
+
+    /// <summary>
+    /// Add a new rewrite context for the specified node.
+    /// </summary>
+    /// <returns>The newly added <see cref="RewriteContext"/>.</returns>
     public RewriteContext NewRewrite(IChildTreeNode node, ulong index)
     {
         Requires.That(!node.InFinalTree, nameof(node), "Cannot be in final tree already.");
-        var context = ContextFor(node.PeekParent()!);
+        var context = ActiveContextFor(node.PeekParent()!);
         Requires.That(context is null || context.AttributeIndex < index, nameof(index), "Must be within the ancestor rewrite.");
-        var newContext = new RewriteContext(context, index);
-        contextNodes[node] = newContext;
-        return newContext;
+        InvalidateCacheFor(node);
+        return contexts[node] = new(context, index);
     }
 
     /// <summary>
@@ -45,52 +60,100 @@ internal sealed class RewriteContexts
         if (rewrittenNode is null) return;
 
         // Node should have its own rewrite context and not need to inherit one
-        if (!contextNodes.TryGetValue(node, out var ctx))
+        if (!contexts.TryGetValue(node, out var ctx))
             throw new InvalidOperationException("Existing node should have a rewrite context.");
 #if DEBUG
         if (node.InFinalTree || !ctx.IsActive || ctx.AttributeIndex != attributeIndex)
             throw new InvalidOperationException("Existing node should not be in the final tree and context should be active for this attribute.");
 #endif
-
-        contextNodes[rewrittenNode] = ctx;
+        InvalidateCacheFor(node);
+        contexts[rewrittenNode] = ctx;
     }
 
     [MethodImpl(MethodImplOptions.AggressiveInlining)]
     public ulong? LowLinkFor(IChildTreeNode node)
-        => ContextFor(node)?.LowLink;
+        => ActiveContextFor(node)?.LowLink;
 
     /// <summary>
-    /// The context for this tree node or <see langword="null"/> for nodes in the final tree.
+    /// Invalidate any cached values for this node and its children.
     /// </summary>
-    private RewriteContext? ContextFor(ITreeNode node)
+    [Inline]
+    private void InvalidateCacheFor(IChildTreeNode node)
     {
-        if (node.InFinalTree)
-            return null;
+        // If a context has been cached for this node then there is a risk that outdated contexts
+        // have been cached for child nodes. Since there is no efficient way to invalidate just the
+        // caches for child nodes, clear the whole cache.
+        if (contextCache.ContainsKey(node))
+            contextCache.Clear();
+    }
 
-        if (contextNodes.TryGetValue(node, out var ctx) && ctx.IsActive)
-            return ctx;
+    /// <summary>
+    /// The active context for this tree node or <see langword="null"/> for nodes without an active
+    /// context.
+    /// </summary>
+    private RewriteContext? ActiveContextFor(ITreeNode node)
+    {
+        var ctx = ContextFor(node, contexts, contextCache);
+        return ctx?.IsActive == true ? ctx : null;
 
-        // If the node is associated with an inactive context, it doesn't need to be removed yet
-        // because it will be replaced with a new context or removed below.
-
-        var parent = node.PeekParent();
-        ctx = parent is not null
-            // Recursively search for the context of the parent node and cache the result.
-            ? ContextFor(parent)
-            // This node is the root of the tree and as such is not part of a rewrite context.
-            : throw new InvalidOperationException($"Root node should already be {nameof(ITreeNode.InFinalTree)}");
-
-        if (ctx is null)
+        // Get the (possibly inactive) context for the given node and update contextCache to cache
+        // contexts for more efficient lookup in the future.
+        static RewriteContext? ContextFor(
+            ITreeNode node,
+            Dictionary<ITreeNode, RewriteContext> contexts,
+            Dictionary<ITreeNode, RewriteContext> contextCache)
         {
-            // This node is above any rewrite context, mark it as final.
-            node.MarkInFinalTree();
-            contextNodes.Remove(node);
+            if (node.InFinalTree)
+                return null;
+
+            // Check if the node is cached
+            if (contextCache.TryGetValue(node, out var cachedContext) && cachedContext.IsActive)
+                return cachedContext;
+
+            // If the node has a cached inactive context, it will be removed or replaced with a new
+            // context below if needed.
+
+            // Not cached, or cached inactive, check if associated with a new active context.
+            if (contexts.TryGetValue(node, out var context) && context.IsActive)
+            {
+                contextCache[node] = context;
+                return context;
+            }
+
+            // Recursively search for the context of the parent node.
+            var parent = node.PeekParent();
+            var parentContext = parent is not null
+                ? ContextFor(parent, contexts, contextCache)
+                // This node is the root of the tree and as such is not part of a rewrite context.
+                : throw new InvalidOperationException($"Root node should already be {nameof(ITreeNode.InFinalTree)}");
+
+            if (parentContext?.IsActive == true)
+            {
+                // This node did not have an active cached context. Update the cache so that future
+                // lookups can find the active context faster.
+                return contextCache[node] = parentContext;
+            }
+
+            // Inherit parent context if no context for node.
+            context ??= parentContext;
+
+            if (context is null)
+                // This node is above any active or inactive rewrite context, mark it as final.
+                node.MarkInFinalTree();
+
+            // In the else case, there is no reason to update the cache. Either the context is
+            // unchanged, or the parent is inactive and so would still require searching up the tree
+            // to look for an active context. Indeed, by not caching we may avoid needing to clear
+            // the cache later.
+
+            return context;
         }
-        else
-            contextNodes[node] = ctx;
-        return ctx;
     }
 
     [MethodImpl(MethodImplOptions.AggressiveInlining)]
-    public void Clear() => contextNodes.Clear();
+    public void Clear()
+    {
+        contextCache.Clear();
+        contexts.Clear();
+    }
 }
